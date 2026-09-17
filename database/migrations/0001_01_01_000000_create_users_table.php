af454c92c6c86910605818890777930af42e6221
Aprillia Abel Cleodora <aprilliaabl17@gmail.com>
2026-09-17 12:01:20 +0700

    test: add SRS-10

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,3 +1,5 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,10 +14,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            
+
             // Kolom TAMBAHAN untuk membedakan Admin dan User
-            $table->enum('role', ['admin', 'user'])->default('user'); 
-            
+            $table->enum('role', ['admin', 'user'])->default('user');
+
             $table->rememberToken();
             $table->timestamps();
         });
